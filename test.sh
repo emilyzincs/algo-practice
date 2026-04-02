459849b12e5680907bca2465a179de4d1c9efce8
Emily Zinschlag <emilyzinschlag@gmail.com>
2026-04-01 23:20:53 -0700

    Can now do ./test.sh all to execute all tests

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
-export SPECIFIC_TEST=$2
-cmd="python -m unittest tests.practice_run_tests."$1".test_"$1"_run_test"
-eval $cmd
+if [ "$1" == "all" ]; then
+  export TEST_SPECIFIER="ALL"
+  cmd="python -m unittest tests.practice_run_tests.abstract_test_run_tests"
+else
+  export TEST_SPECIFIER=$2
+  cmd="python -m unittest tests.practice_run_tests."$1".test_"$1"_run_test"
+fi
+eval "$cmd"
--- tests/practice_run_tests/abstract_test_run_tests.py
+++ tests/practice_run_tests/abstract_test_run_tests.py
@@ -3,9 +3,9 @@ from unittest.mock import patch
 import os
 from utils import is_type
 from commands.practice.practice import run_tests
-from get_file_paths import PROJECT_ROOT, get_solution_file_dir, get_solution_file_path
+from get_file_paths import PROJECT_ROOT, get_solution_file_dir, get_solution_file_path, to_language_file_case
 from typing import Optional
-from app import ALG_LIST
+from app import ALG_LIST, LANGUAGE_LIST, LANGUAGE_TO_EXTENSION_AND_COMMENT_SYMBOL
 
 UNIT_TESTS = {"run_tests", "solution"}
 
@@ -14,7 +14,7 @@ class AbstractTestRunTests(unittest.TestCase):
     super().__init__(*args, **kwargs)
     self.gfp_base = "commands.practice.practice.gfp."
     self.debug = False
-    self.specific_test = os.getenv("SPECIFIC_TEST").strip().lower()
+    self.test_specifier = os.getenv("TEST_SPECIFIER").strip().lower()
 
   def abstract_test_run_tests(
       self,
@@ -24,8 +24,8 @@ class AbstractTestRunTests(unittest.TestCase):
       practice_file_name_prefix: str,
       required_class_name_prefix: Optional[str] = None,
   ):
-    if self.specific_test and not (self.specific_test == "run_tests"
-                                  or is_type(self.specific_test, int)):
+    if self.test_specifier and not (self.test_specifier == "run_tests"
+                                  or is_type(self.test_specifier, int)):
       print("\n\nSKIPPING " + language.upper() + " run_test TESTS.")
       return
     print("\n\nRUNNING " + language.upper() + " run_test TESTS.")
@@ -57,7 +57,7 @@ class AbstractTestRunTests(unittest.TestCase):
       True, # 25
       False,
     ]
-    test_number = int(self.specific_test) if self.specific_test and is_type(self.specific_test, int) else None
+    test_number = int(self.test_specifier) if self.test_specifier and is_type(self.test_specifier, int) else None
     json_path_prefix = self.get_json_path_prefix()
     practice_file_prefix = os.path.join(practice_file_dir, practice_file_name_prefix)
     i = test_number if test_number is not None else 1
@@ -73,7 +73,7 @@ class AbstractTestRunTests(unittest.TestCase):
         if not os.path.exists(json_path) or test_number and i != test_number:
           print(f"Done.")
           break
-        print(f"\nRunning test {i}:")
+        print(f"\nRunning {language} test {i}:")
         with (
           patch(self.gfp_base + "get_test_file_path", return_value=json_path),
           patch(self.gfp_base + "get_practice_file_path", return_value=practice_file_path)
@@ -95,16 +95,16 @@ class AbstractTestRunTests(unittest.TestCase):
       language: str,
       extension: str   
   ) -> None:
-    if self.specific_test and not (self.specific_test == "solution" 
-                                  or self.specific_test in ALG_LIST):
+    if self.test_specifier and not (self.test_specifier == "solution" 
+                                  or self.test_specifier in ALG_LIST):
       print("\nSKIPPING " + language.upper() + " ALGORITHM SOLUTION TESTS.")
       return
     print("\n\nRUNNING " + language.upper() + " ALGORITHM SOLUTION TESTS.")
-    specific_alg = self.specific_test if (self.specific_test and
-                                          self.specific_test != "solution") else None
+    specific_alg = self.test_specifier if (self.test_specifier and
+                                          self.test_specifier != "solution") else None
 
     def abstract_test_alg_solution(alg: str, language: str, extension: str) -> None:
-      print(f"\nTesting solution for {alg}.")
+      print(f"\nTesting {language} solution for {alg}.")
       solution_file_dir = get_solution_file_dir(alg)
       solution_file = get_solution_file_path(alg, language, extension)
       self.assertEqual(True, os.path.exists(solution_file_dir), f"Path {solution_file_dir} does not exist.")
@@ -126,6 +126,26 @@ class AbstractTestRunTests(unittest.TestCase):
       for alg in ALG_LIST:
         abstract_test_alg_solution(alg, language, extension)
     print("Done.")
+
+  def test_all(self) -> None:
+    if self.test_specifier != "all":
+      return
+    self.test_specifier = ""
+    for language in LANGUAGE_LIST:
+      extension = LANGUAGE_TO_EXTENSION_AND_COMMENT_SYMBOL[language][0]
+      practice_file_name_prefix = to_language_file_case("sol", language)
+      required_class_name_prefix = None
+      practice_file_dir = os.path.join(PROJECT_ROOT, "tests", "practice_run_tests", language, "solution_files")
+      if language == "java":
+        required_class_name_prefix = "Sol"
+      self.abstract_test_run_tests(
+        language,
+        extension,
+        practice_file_dir,
+        practice_file_name_prefix,
+        required_class_name_prefix
+      )
+      self.abstract_test_solutions(language, extension)
 
   def get_json_path_prefix(self) -> str:
     return os.path.join(PROJECT_ROOT, "tests", "practice_run_tests", "json_files", "test")
