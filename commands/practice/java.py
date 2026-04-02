f1e1270b7f0ab1150967625dd44090974632c55e
Emily Zinschlag <emilyzinschlag@gmail.com>
2026-04-02 00:24:42 -0700

    Fixed error where commands/practice was gitignored

---
+++ commands/practice/java.py
@@ -0,0 +1,77 @@
+import os, sys, subprocess
+from get_file_paths import PROJECT_ROOT
+from typing import List
+from pathlib import Path
+
+def get_test_cmd(
+    alg: str,
+    practice_file_dir: str,
+    practice_file: str,
+    test_runner_dir: str,
+    test_runner_file: str,
+    test_file: str
+) -> List[str]|int:
+  try:
+    compile_if_necessary(practice_file, practice_file_dir)
+  except RuntimeError as e:
+    print(f"User code compilation failed:\n{e}", file=sys.stderr)
+    return -1
+  additional_dependencies = [practice_file_dir]
+  try:
+    compile_if_necessary(test_runner_file, test_runner_dir, additional_dependencies) 
+  except RuntimeError as e:
+    print(f"Failed to compile java test runner:\n")
+    raise e
+  runtime_cp_entries =[
+    PROJECT_ROOT,
+    test_runner_dir,
+    practice_file_dir,
+  ]
+  add_jars(runtime_cp_entries, test_runner_dir)
+  additional_args = [alg, test_file, path_to_package(practice_file_dir, PROJECT_ROOT)]
+  class_path = os.path.join(test_runner_dir, "Runner")
+  class_path_for_cmd = path_to_package(class_path, PROJECT_ROOT)
+  return ["java", "-cp", os.pathsep.join(runtime_cp_entries), 
+          class_path_for_cmd] + additional_args
+
+def file_to_class_path(file: str) -> str:
+  if not file.endswith(".java"):
+    raise ValueError("file path must end in .java! given:", file)
+  return file[:-len(".java")] + ".class"
+
+def compile_if_necessary(java_file: str, cwd: str, 
+                         additional_dependencies: List[str] = None) -> None:
+  if not os.path.exists(java_file):
+    raise ValueError(f"Could not find {java_file}")
+
+  class_path = file_to_class_path(java_file)
+  if os.path.exists(class_path) and os.path.getmtime(java_file) > os.path.getmtime(class_path):
+    os.remove(class_path)
+  if not os.path.exists(class_path): 
+    cp_entries = [cwd]
+    add_jars(cp_entries, cwd)
+    if additional_dependencies:
+      for d in additional_dependencies:
+        cp_entries.append(d)
+    cp = os.pathsep.join(cp_entries)
+    compile_cmd = ["javac", "-cp", cp, java_file]
+    result = subprocess.run(compile_cmd, cwd=cwd, capture_output=True, text=True) 
+    if result.returncode == 0 and not os.path.exists(class_path):
+      raise RuntimeError(f"Failed to compile into file {class_path}." +
+                         " ensure the .java file contains a public class with the same name as the file")
+    if result.returncode != 0:
+      raise RuntimeError(f"Compilation failed:\n{result.stderr}")
+  
+def add_jars(cp_entries: List[str], dir: str) -> None:
+  lib_dir = os.path.join(dir, "lib")
+  if os.path.exists(lib_dir):
+    for f in Path(lib_dir).iterdir(): 
+      if f.name.endswith(".jar"):
+        cp_entries.append(os.path.join(lib_dir, f.name))
+
+def path_to_package(path: str, project_root_path: str) -> str:
+  path = path.replace(project_root_path, "")
+  path = path.replace(os.sep, "", 1)
+  path = path.replace(os.sep, ".")
+  return path
+      
---
+++ commands/practice/practice.py
@@ -0,0 +1,84 @@
+import sys
+import get_file_paths as gfp
+import subprocess
+from commands.practice.java import get_test_cmd as java_get_test_cmd
+from utils import print_desc, in_either
+from commands.command_util import GLOBAL_COMMANDS, handle_global_command, get_global_command_descriptions
+
+def handle_commands(
+    local_commands: set[str],
+    alg: str,
+    language: str,
+    extension: str,
+    delete_attempts_func,
+    load_solution_func,
+    exit_func
+) -> bool:
+  correct = False
+  while not correct:
+    user_input = input("Type 'done' when you are finished or 'help' for options:\n")
+    if not in_either(user_input, GLOBAL_COMMANDS, local_commands):
+      print("Unrecognized input. Type 'help' to see valid inputs.", file=sys.stderr)
+      continue
+    
+    if user_input in GLOBAL_COMMANDS:
+      if not handle_global_command(user_input, handle_help, exit_func):
+        delete_attempts_func()
+        return False
+    else:
+      match user_input:
+        case "d" | "done":
+          correct = run_tests(alg, language, extension)
+        case "s" | "sol" | "solution":
+          try:
+            load_solution_func(alg)
+          except FileNotFoundError:
+            print(f"Cannot load solution because it does not exist.", file=sys.stderr)
+        case _:
+          raise ValueError(f"Unhandled case {user_input}.")
+  delete_attempts_func()    
+  return True
+
+def handle_help():
+  command_descriptions = get_global_command_descriptions()
+  command_descriptions.extend([
+    "d/done: Submits the current practice implementation to be tested",
+    "s/sol/solution: Loads the algorithm solution into the file"
+  ])
+  print("This menu supports the following inputs:")
+  print_desc(command_descriptions) 
+
+def run_tests(
+    alg: str,
+    language: str, 
+    extension: str,
+    show_subprocess_text: bool = True,
+    required_class_name: str = "Solution",
+    required_method_name: str = "solve"
+  ) -> bool:
+  practice_file_dir = gfp.get_practice_file_dir()
+  practice_file = gfp.get_practice_file_path(language, extension)
+  test_runner_dir = gfp.get_test_runner_dir_path(language)
+  test_runner_file = gfp.get_test_runner_file_path(language, extension)
+  test_file = gfp.get_test_file_path(alg)
+  match language:
+    case "python":
+      cmd = ["python", test_runner_file, practice_file, test_file, gfp.PROJECT_ROOT]
+    case "java":
+      cmd = java_get_test_cmd(
+        alg,
+        practice_file_dir,
+        practice_file,
+        test_runner_dir,
+        test_runner_file,
+        test_file
+      )
+    case _:
+      raise NameError("Could not find language:", language)
+  if isinstance(cmd, int):
+    return False
+  cmd.extend([required_class_name, required_method_name])
+  result = subprocess.run(cmd, cwd=test_runner_dir, capture_output=not show_subprocess_text)
+  if result.returncode != 0:
+    return False
+  return True
