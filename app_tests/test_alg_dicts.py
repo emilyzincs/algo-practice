6ce42d57e8cbae86e08cebacf75c87714bcdf03a
Emily Zinschlag <emilyzinschlag@gmail.com>
2026-04-06 22:17:49 -0700

    Tests pass

--- app_tests/test_alg_dicts.py
+++ app_tests/test_alg_dicts.py
@@ -5,14 +5,17 @@ from util.enums import (
 
 class TestAlgDicts(unittest.TestCase):
   def test_input_alg_to_specific(self) -> None:
+    print("\n\nTESTING INPUT_ALG_TO_SPECIFIC.")
     seen: set[SpecificAlgorithm] = set()
     for input_alg, specific_alg in INPUT_ALG_TO_SPECIFIC.items():
       self.assertEqual(str, type(input_alg))
       self.assertEqual(SpecificAlgorithm, type(specific_alg))
       seen.add(specific_alg)
     self.assertEqual(len(SpecificAlgorithm), len(seen))
+    print("Done.")
 
   def test_specific_alg_to_general(self) -> None:
+    print("\n\nTESTING INPUT_ALG_TO_SPECIFIC.")
     specific_seen: set[SpecificAlgorithm] = set()
     general_seen: set[GeneralAlgorithm] = set()
     for specific_alg, general_alg in SPECIFIC_ALG_TO_GENERAL.items():
@@ -22,4 +25,5 @@ class TestAlgDicts(unittest.TestCase):
       general_seen.add(general_alg)
     self.assertEqual(len(SpecificAlgorithm), len(specific_seen))
     self.assertEqual(len(GeneralAlgorithm), len(general_seen))
+    print("Done.")
 
