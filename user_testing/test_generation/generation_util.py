b0a4956304f1ce522dd974459eeab07263d1854a
Emily Zinschlag <emilyzinschlag@gmail.com>
2026-04-14 23:45:31 -0700

    Rough draft abstraction of graph test case generation

--- user_testing/test_generation/generation_util.py
+++ user_testing/test_generation/generation_util.py
@@ -1,5 +1,5 @@
 import random
-from typing import Any
+from typing import Any, Callable
 
 
 def get_null() -> None:
@@ -83,70 +83,3 @@ def trim_tree(arr: list[int]) -> list[int]:
   while arr and arr[-1] is None:
     arr.pop()
   return arr
-
-
-def rand_graph(
-    num_vertices: int, 
-    directed: bool, 
-    connected: bool, 
-    edge_prob: float = 0.2
-) -> list[list[int]]:
-  
-  graph: list[list[int]] = [[] for _ in range(num_vertices)]
-  for i in range(num_vertices):
-    for j in range(i+1, num_vertices):
-      if rand_bool(edge_prob):
-        graph[i].append(j)
-        if directed and rand_bool(edge_prob):
-          graph[j].append(i)
-        elif not directed:
-          graph[j].append(i)
-  if connected:
-    connect_graph(graph, directed) 
-      
-  return graph
-
-
-def graph_list_to_tuple(graph: list[list[int]]) -> tuple[tuple[int, ...], ...]:
-  return tuple(tuple(adj_list) for adj_list in graph)
-
-
-def rand_vertex(graph: list[list[int]]) -> int:
-  n = len(graph)
-  if n == 0:
-    raise ValueError("Cannot pick random vertex since graph has no vertices")
-  return random.randint(0, n-1)
-
-
-def rand_graph_and_root(num_vertices: int, directed: bool, connected: bool, 
-                       edge_prob: float) -> tuple[list[list[int]], int]: 
-  graph = rand_graph(num_vertices, directed, connected,  edge_prob)
-  root = rand_vertex(graph)
-  return graph, root
-
-
-def connect_graph(graph: list[list[int]], directed: bool) -> None:
-  n = len(graph)
-  if n == 0:
-    return
-  for i in range(1, n):
-    if i not in graph[i-1]:
-      graph[i-1].append(i)
-    if not directed and i-1 not in graph[i]:
-      graph[i].append(i-1)
-  if 0 not in graph[n-1]:
-    graph[n-1].append(0)
-  if not directed and n-1 not in graph[0]:
-    graph[0].append(n-1)
-
-
-def weight_graph(unweighted_graph: list[list[int]], lo: int, hi: int
-                 ) -> list[list[tuple[int, float]]]:
-  weighted_graph: list[list[tuple[int, float]]] = []
-  for unweighted_neighbor_list in unweighted_graph:
-      weighted_neighbor_list = [(neighbor, random.uniform(lo, hi)) 
-                                  for neighbor in unweighted_neighbor_list]
-      weighted_graph.append(weighted_neighbor_list)
-  return weighted_graph
-      
-
--- user_testing/test_generation/problems/reachable.py
+++ user_testing/test_generation/problems/reachable.py
@@ -1,5 +1,6 @@
 from problems.reachable.breadth_first_search.solution import Solution
-import user_testing.test_generation.generation_util as util
+
+from user_testing.test_generation.graph_util import *
 from user_testing.test_generation.base_generator import BaseGenerator
 from util.enums import GeneralAlgorithm
 from typing import override
@@ -13,19 +14,12 @@ sol = Solution()
 class ReachableGenerator(BaseGenerator):
 
   @override
-  def get_all_test_cases(self) -> list[tuple[list[list[int]], int]]:
+  def get_all_test_cases(self) -> list[tuple[UnweightedGraph, int]]:
     test_cases = self.get_edge_cases()
-    for i in range(3, 8):
-      self.add_random_variety(test_cases, i, 2)
-    for i in range(15, 18):
-      self.add_random_variety(test_cases, i, 2)
-    for i in range(49, 52):
-      self.add_random_variety(test_cases, i, 3)
-    for i in range(99, 102):
-      self.add_random_variety(test_cases, i, 3)
-    self.add_random_variety(test_cases, 250, 3)
-    test_cases = self.remove_duplicate_test_cases(test_cases)
-    return test_cases
+    unweighted_graphs: list[UnweightedGraph] = get_unweighted_graphs(
+      directed=True, connected=False)
+    test_cases.extend(get_graphs_with_rand_vertex(unweighted_graphs))
+    return self.remove_duplicate_test_cases(test_cases)
   
   @override
   def oracle(self, graph: list[list[int]], root: int) -> list[int]:
@@ -48,30 +42,8 @@ class ReachableGenerator(BaseGenerator):
       ([[0], [0, 1]], 1),
     ]
 
-  def add_random_cases(self, test_cases: list[tuple[list[list[int]], int]], 
-                       num_vertices: int, directed: bool, 
-                       edge_prob: float, num_cases: int) -> None:
-    for _ in range(num_cases):
-      test_cases.append(util.rand_graph_and_root(num_vertices, directed, edge_prob))
-
-  def add_random_variety(self, test_cases, num_vertices: int, num_cases: int) -> None:
-    edge_probs = None
-    if num_vertices < 20:
-      edge_probs = [0, 0.3, 0.5, 0.7, 1]
-    elif num_vertices < 50:
-      edge_probs = [0, 0.1, 0.3, 0.5, 0.7, 0.9, 1]
-    else:
-      edge_probs = [0, 0.05, 0.15, 0.3, 0.5, 0.7, 0.85, 0.95, 1]
-    for directed in [True, False]:
-      for edge_prob in edge_probs:
-        self.add_random_cases(test_cases, num_vertices, directed, edge_prob, num_cases)
-        if edge_prob <= 0.3:
-          graph, root = util.rand_graph_and_root(num_vertices, directed, edge_prob)
-          util.connect_graph(graph, directed)
-          test_cases.append((graph, root))
-
-  def remove_duplicate_test_cases(self, test_cases: list[tuple[list[list[int]], int]]
-                                  ) -> list[tuple[list[list[int]], int]]:
+  def remove_duplicate_test_cases(self, test_cases: list[tuple[UnweightedGraph, int]]
+                                  ) -> list[tuple[UnweightedGraph, int]]:
     hashable_cases = []
     for graph, root in test_cases:
       graph_tuple = tuple(tuple(adj_list) for adj_list in graph)
