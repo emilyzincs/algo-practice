8f57f3ee41e25acd5fd6f51f3fe89a6da3af7d8e
Emily Zinschlag <emilyzinschlag@gmail.com>
2026-04-16 23:26:50 -0700

    Implemented first pass at test generation for max flow

--- problems/max_flow/ford_fulkerson/solution.py
+++ problems/max_flow/ford_fulkerson/solution.py
@@ -1,3 +1,3 @@
 class Solution:
-  def solve(self, graph: list[list[tuple[int, int]]], start: int, target: int) -> int:
+  def solve(self, graph: list[list[tuple[int, int]]], s: int, t: int) -> int:
     raise NotImplementedError()
