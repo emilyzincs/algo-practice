4724d42f8bfb3d39be01960de729cef865b0dc2c
Emily Zinschlag <emilyzinschlag@gmail.com>
2026-04-20 11:47:10 -0700

    First try at updating file_paths.py

--- util/file_paths.py
+++ util/file_paths.py
@@ -2,11 +2,10 @@ import os
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 from typing import assert_never
 from util.enums import (
+  DirectoryType,
   SpecificAlgorithm,
   GeneralAlgorithm,
   AlgorithmCategory,
-  SPECIFIC_ALG_TO_GENERAL,
-  GENERAL_ALG_TO_CATEGORY,
   Language,
   member_to_string
 )
@@ -35,62 +34,101 @@ def to_language_file_case(string: str, lang: Language) -> str:
 def get_practice_file_dir() -> str:
   return os.path.join(PROJECT_ROOT, "practice")
 
+
 def get_practice_file_path(lang: Language) -> str:
   return os.path.join(get_practice_file_dir(), 
                       to_language_file_case("solution", lang) + lang.extension)
 
-def get_solution_file_dir(specific_alg: SpecificAlgorithm) -> str:
-  gen_alg: GeneralAlgorithm = SPECIFIC_ALG_TO_GENERAL[specific_alg]
-  if gen_alg.name == specific_alg.name:
-    return os.path.join(PROJECT_ROOT, "problems", member_to_string(specific_alg))
-  else:
-    return os.path.join(PROJECT_ROOT, "problems", member_to_string(gen_alg), 
+
+def get_problems_dir() -> str:
+  return os.path.join(PROJECT_ROOT, "problems")
+
+
+def get_specific_alg_dir(specific_alg: SpecificAlgorithm) -> str:
+  gen_alg: GeneralAlgorithm = specific_alg.general_type
+  category: AlgorithmCategory = gen_alg.category
+
+  return os.path.join(get_problems_dir(),
+                        member_to_string(category),
+                        member_to_string(gen_alg), 
                         member_to_string(specific_alg))
+
+
+def get_solution_file_dir(specific_alg: SpecificAlgorithm) -> str:
+  return get_specific_alg_dir(specific_alg)
+
 
 def get_solution_file_path(alg: SpecificAlgorithm, lang: Language) -> str:
   return os.path.join(get_solution_file_dir(alg),
                       to_language_file_case("solution", lang) + lang.extension)
 
-def get_gen_alg_dir(alg: GeneralAlgorithm) -> str:
-  return os.path.join(PROJECT_ROOT, "problems", member_to_string(alg))
 
-def specific_alg_to_info_path(specific_alg: SpecificAlgorithm) -> str:
-  gen_alg: GeneralAlgorithm = SPECIFIC_ALG_TO_GENERAL[specific_alg]
-  return general_alg_to_info_path(gen_alg)
+def get_category_dir(alg: SpecificAlgorithm):
+  return os.path.join(get_problems_dir(), member_to_string(alg.category))
 
-def general_alg_to_info_path(gen_alg: GeneralAlgorithm) -> str:
-  return os.path.join(get_gen_alg_dir(gen_alg), "info.json")
 
-def specific_alg_to_test_path(specific_alg: SpecificAlgorithm) -> str:
-  gen_alg: GeneralAlgorithm = SPECIFIC_ALG_TO_GENERAL[specific_alg]
-  return general_alg_to_test_path(gen_alg)
+def get_gen_alg_dir(alg: SpecificAlgorithm) -> str:
+  return os.path.join(get_category_dir(alg), member_to_string(alg.general_type))
 
-def general_alg_to_test_path(gen_alg: GeneralAlgorithm) -> str:
-  return os.path.join(get_gen_alg_dir(gen_alg), "test.json")
+
+def get_info_path(alg: SpecificAlgorithm) -> str:
+  dir_type: DirectoryType = alg.info_dir
+  dir_path: str
+  match dir_type:
+    case DirectoryType.SPECIFIC:
+      dir_path = get_specific_alg_dir(alg)
+    case DirectoryType.GENERAL:
+      dir_path = get_gen_alg_dir(alg)
+    case _:
+      assert_never(dir_type)
+  return os.path.join(dir_path, "info.json")
+
+
+def get_test_path(alg: SpecificAlgorithm) -> str:
+  dir_type: DirectoryType = alg.test_dir
+  dir_path: str
+  match dir_type:
+    case DirectoryType.SPECIFIC:
+      dir_path = get_specific_alg_dir(alg)
+    case DirectoryType.GENERAL:
+      dir_path = get_gen_alg_dir(alg)
+    case _:
+      assert_never(dir_type)
+  return os.path.join(dir_path, "test.json")
+
 
 def get_test_runner_dir_path(lang: Language) -> str:
-  return os.path.join(PROJECT_ROOT, "user_testing", "test_runners", member_to_string(lang))
+  return os.path.join(PROJECT_ROOT, "user_testing", 
+                      "test_runners", member_to_string(lang))
+
 
 def get_test_runner_file_path(lang: Language) -> str:
   return os.path.join(get_test_runner_dir_path(lang), 
                       to_language_file_case("runner", lang) + lang.extension)
 
+
 def get_settings_path() -> str:
   return os.path.join(PROJECT_ROOT, "user_settings", "current.json")
+
 
 def get_default_settings_path() -> str:
   return os.path.join(PROJECT_ROOT, "user_settings", "default.json")
 
+
 def get_abstract_test_dir() -> str:
   return os.path.join(PROJECT_ROOT, "app_tests")
 
+
 def get_test_generator_path(specific_alg: SpecificAlgorithm) -> str:
-  gen_alg = SPECIFIC_ALG_TO_GENERAL[specific_alg]
-  return os.path.join(PROJECT_ROOT, "user_testing", "test_generation", "problems", 
-                      member_to_string(gen_alg) + ".py")
+  raise NotImplementedError()
+  # gen_alg = SPECIFIC_ALG_TO_GENERAL[specific_alg]
+  # return os.path.join(PROJECT_ROOT, "user_testing", "test_generation", "problems", 
+  #                     member_to_string(gen_alg) + ".py")
+
 
 def get_boilerplate_dir() -> str:
   return os.path.join(PROJECT_ROOT, "boilerplate")
+
 
 def get_boilerplate_language_file_path(lang: Language) -> str:
   return os.path.join(get_boilerplate_dir(), "language", 
